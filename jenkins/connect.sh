7b82452c91224ffaaf356555347b0098b06ae1c9
Andrea Valenzuela <aand.valenzuela@gmail.com>
2023-03-16 17:27:57 +0100

    Rename "slave" references to "node"

--- jenkins/connect.sh
+++ jenkins/connect.sh
@@ -13,29 +13,29 @@ fi
 KTAB=${HOME}/keytabs/$(echo $TARGET | sed 's|@.*||').keytab
 if [ ! -f $KTAB ] ; then KTAB=${HOME}/keytabs/cmsbld.keytab ; fi
 KINIT_USER=$(klist -k -t -K ${KTAB} | sed  's|@CERN.CH.*||;s|.* ||' | tail -1)
-export SLAVE_TYPE=$(echo $TARGET | sed 's|^.*@||;s|[.].*||')
-export KRB5CCNAME=FILE:/tmp/krb5cc_$(id -u)_${KINIT_USER}_${SLAVE_TYPE}
+export NODE_TYPE=$(echo $TARGET | sed 's|^.*@||;s|[.].*||')
+export KRB5CCNAME=FILE:/tmp/krb5cc_$(id -u)_${KINIT_USER}_${NODE_TYPE}
 KPRINCIPAL=${KINIT_USER}@CERN.CH
 kinit ${KPRINCIPAL} -k -t ${KTAB}
 klist || true
 export SLAVE_UNIQUE_TARGET=""
-export SLAVE_MAX_WORKSPACE_SIZE=""
+export NODE_MAX_WORKSPACE_SIZE=""
 SCRIPT_DIR=$(cd $(dirname $0); /bin/pwd)
 BLACKLIST_DIR="${HOME}/workspace/cache/blacklist"
 
-if [ $(echo $SLAVE_TYPE | grep '^lxplus\|^aiadm' | wc -l) -gt 0 ] ; then
+if [ $(echo $NODE_TYPE | grep '^lxplus\|^aiadm' | wc -l) -gt 0 ] ; then
   export SLAVE_UNIQUE_TARGET="YES"
-  case ${SLAVE_TYPE} in 
-    lxplus* ) export SLAVE_MAX_WORKSPACE_SIZE=10;;
+  case ${NODE_TYPE} in 
+    lxplus* ) export NODE_MAX_WORKSPACE_SIZE=10;;
   esac
-  for ip in $(host $SLAVE_TYPE | grep 'has address' | sed 's|^.* ||'); do
+  for ip in $(host $NODE_TYPE | grep 'has address' | sed 's|^.* ||'); do
     hname=$(host $ip | grep 'domain name' | sed 's|^.* ||;s|\.$||')
     if [ $(grep $(echo "${hname}" | cut -d "." -f 1) ${SCRIPT_DIR}/blacklist-lxplus.txt | wc -l) -gt 0 ] ; then continue ; fi
     if [ -e ${BLACKLIST_DIR}/${hname} ] ; then continue ; fi
     NEW_TARGET=$(echo $TARGET | sed "s|@.*|@$hname|")
-    ${SCRIPT_DIR}/start-slave.sh "${NEW_TARGET}" "$@" || [ "X$?" = "X99" ] && sleep 5 && continue
+    ${SCRIPT_DIR}/start-node.sh "${NEW_TARGET}" "$@" || [ "X$?" = "X99" ] && sleep 5 && continue
     exit 0
   done
   exit 1
 fi
-${SCRIPT_DIR}/start-slave.sh "${TARGET}" "$@"
+${SCRIPT_DIR}/start-node.sh "${TARGET}" "$@"
