6f35b80e0d314d9f4fef8d655396cde85f83e0cb
Andrea Valenzuela <aand.valenzuela@gmail.com>
2023-11-16 16:53:46 +0100

    Update github-rate-limits.py

--- github-rate-limits.py
+++ github-rate-limits.py
@@ -19,6 +19,7 @@ if __name__ == "__main__":
     print("Limit: ", limit)
     reset_time = datetime.fromtimestamp(gh.rate_limiting_resettime)
     print("Reset time (GMT): ", reset_time)
+    print("User: ", gh.get_user)
 
     JENKINS_PREFIX = "jenkins"
     try:
