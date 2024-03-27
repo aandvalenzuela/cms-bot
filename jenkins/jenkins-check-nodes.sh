744e7ed062c060b8cd497b6f704fdd3f792ebcae
Andrea Valenzuela <aand.valenzuela@gmail.com>
2024-03-27 10:34:22 +0100

    Update jenkins-check-nodes.sh

--- jenkins/jenkins-check-nodes.sh
+++ jenkins/jenkins-check-nodes.sh
@@ -21,7 +21,13 @@ function run_check {
     node=$1
     SSH_OPTS="-q -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=60"
     scp $SSH_OPTS ${WORKSPACE}/cms-bot/jenkins/nodes-sanity-check.sh "cmsbuild@$node:/tmp" || (echo "Cannot scp script" && exit 1)
-    ssh $SSH_OPTS "cmsbuild@$node sh /tmp/nodes-sanity-check.sh $SINGULARITY $PATHS >> /tmp/logfile && (scp $SSH_OPTS cmsbuild@$node:/tmp/logfile cmsbuild@cmsjenkins03:$HOME/workspace/cache/logfile) && (rm /tmp/logfile)"; exit_code=$?
+    #ssh $SSH_OPTS "(cmsbuild@$node sh /tmp/nodes-sanity-check.sh $SINGULARITY $PATHS >> /tmp/logfile) && (scp $SSH_OPTS cmsbuild@$node:/tmp/logfile cmsbuild@cmsjenkins03:$HOME/workspace/cache/logfile) && (rm /tmp/logfile)"; exit_code=$?
+    ssh $SSH_OPTS "cmsbuild@$node" "sh /tmp/nodes-sanity-check.sh $SINGULARITY $PATHS >> /tmp/logfile"
+    echo "ssh done"
+    scp $SSH_OPTS "cmsbuild@$node:/tmp/logfile" "cmsbuild@cmsjenkins03:$HOME/workspace/cache/logfile"
+    echo "scp log done"
+    ssh $SSH_OPTS "cmsbuild@$node" "rm /tmp/logfile"
+    echo "cleanup done"
     echo "=> Exit code: $exit_code"
     cat $HOME/workspace/cache/logfile
     if [[ ${exit_code} -eq 0 ]]; then
