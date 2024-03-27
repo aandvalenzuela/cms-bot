8de8533ec60c91fe4fb1ae706ef4d861b911c579
Andrea Valenzuela <aand.valenzuela@gmail.com>
2024-03-27 10:40:15 +0100

    Update jenkins-check-nodes.sh

--- jenkins/jenkins-check-nodes.sh
+++ jenkins/jenkins-check-nodes.sh
@@ -21,8 +21,10 @@ function run_check {
     node=$1
     SSH_OPTS="-q -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=60"
     scp $SSH_OPTS ${WORKSPACE}/cms-bot/jenkins/nodes-sanity-check.sh "cmsbuild@$node:/tmp" || (echo "Cannot scp script" && exit 1)
-    ssh $SSH_OPTS "cmsbuild@"$node "sh /tmp/nodes-sanity-check.sh $SINGULARITY $PATHS" >> $WORKSPACE/logfile; exit_code=$?
+    ssh $SSH_OPTS "cmsbuild@"$node "sh /tmp/nodes-sanity-check.sh $SINGULARITY $PATHS" >> $WORKSPACE/logfile
+    echo "Done"
     cat $WORKSPACE/logfile
+    echo "Exit"
     exit 1
     if [[ ${exit_code} -eq 0 ]]; then
         rm -f "$blacklist_path/$node"
