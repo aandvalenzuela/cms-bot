2a8074f74172a34749d6f087dd1846ec05c7caca
Andrea Valenzuela <aand.valenzuela@gmail.com>
2024-05-22 11:05:20 +0200

    Set environment

--- lumi/submit.sh
+++ lumi/submit.sh
@@ -1,7 +1,22 @@
 #!/bin/bash -e
 #########################################
 
-env
+# work around a problem with the ROCm device permissions
+export ROCM_TARGET_LST=${HOME}/.rocm_targets
+echo "gfx90a" > ${ROCM_TARGET_LST}
+
+# configure Singularity and CVMFS
+export SINGULARITY_CACHEDIR=/project/$SLURM_ACCOUNT/singularity
+export SINGULARITY_CMSSWDIR=/project/$SLURM_ACCOUNT/cmssw
+export SINGULARITY_PROMPT='[\D{%Y-%m-%d %H:%M:%S}] \[\e]0;\u@\h: \w\a\]\[\033[01;34m\]\u@\h\[\033[00m\]:\[\033[38;5;27m\]\w\[\033[00m\]\$ '
+export SINGULARITY_TMPDIR=$TMP/singularity
+
+export SINGULARITY_SCRATCH=/scratch/$SLURM_ACCOUNT/cmsbuild/workspace.ext3
+export SINGCVMFS_REPOSITORIES=cms.cern.ch,cms-ib.cern.ch,cms-ci.cern.ch,grid.cern.ch,unpacked.cern.ch,patatrack.cern.ch
+export SINGSINGCVMFS_CACHEIMAGEIMAGE=/project/$SLURM_ACCOUNT/cmsbuild/cvmfscache.ext3
+
+ls $SINGULARITY_SCRATCH
+ls $SINGULARITY_TMPDIR
 
 echo "#########################################"
 
