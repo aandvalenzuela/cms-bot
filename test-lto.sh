daf8e0ddd1688299c324f7d0f62213c2213be4a1
aandvalenzuela <andrea.valenzuela.ramirez@cern.ch>
2024-10-15 15:50:22 +0200

    [revert] allow upgrade workflows

--- test-lto.sh
+++ test-lto.sh
@@ -66,7 +66,7 @@ cd ${IB} && cmsenv
 scram build -j 16
 echo "*** RUNNING WF TO DUMP CONFIG FILES ***"
 mkdir relvals && mkdir data && cd data
-runTheMatrix.py -l $WF -t ${THREADS} -w upgrade --ibeos --job-reports  --command "  --customise Validation/Performance/TimeMemorySummary.customiseWithTimeMemorySummary"
+runTheMatrix.py -l $WF -t ${THREADS} --ibeos --job-reports  --command "  --customise Validation/Performance/TimeMemorySummary.customiseWithTimeMemorySummary"
 cp -r ${WF}*/*.py ../relvals
 cd ${WF}*
 
