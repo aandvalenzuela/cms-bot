552d5a40a06c5432a57fd1dd06d7e648f16f8f6f
Andrea Valenzuela <aand.valenzuela@gmail.com>
2024-10-28 07:33:34 +0100

    Update test-lto.sh

--- test-lto.sh
+++ test-lto.sh
@@ -122,7 +122,7 @@ fi
 cd ../../relvals
 
 echo "*** RUNNING WF STEPS ***"
-for x in 1 2 3 4 5; do
+for x in 1 2 3; do
   echo "--------- NEW RUN ------------"
   step=0
   for files in $(ls *.py); do
